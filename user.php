fb61f74b443bb4fe10c154e2a511617f4eb7e286
ACH-S-M <MuhamadihsanM101@gmail.com>
2025-02-22 10:02:17 +0700

    Udah ada Image CartNya

--- user.php
+++ user.php
@@ -1,13 +1,41 @@
 <?php
+include 'koneksi.php';
+$Gambar = null;
+$querySelect = "SELECT Judul_Buku,Author,Harga,ImgRes FROM Buku";
+$hasil = mysqli_query($konek,$querySelect);
+while ($data = mysqli_fetch_assoc($hasil)){
+    $judulBuku = $data['Judul_Buku'];
+    $author = $data['Author'];
+    $Harga = $data['Harga'];
+    $ImgResource = base64_encode( $data['ImgRes']);
+    echo "
+    <div class='container'>
+        <div class='card'>
+                <img src='data:image/jpeg;base64,$ImgResource'>
+                    <div class='detail'>
+                            <h3>$judulBuku</h3>
+                            <B>$Harga</B>
+                            <p>$author</p>
+                            <Button>Beli</Button>
+                    </div>
+        </div>
+    </div>";
 
+}
+
+
+  
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
+    <link rel="stylesheet" href="mystyle.css">
+    <style>
+        
+    </style>
 </head>
 <body>
-    <H1>User Nih</H1>
+    
 </body>
 </html>
